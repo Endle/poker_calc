4e34be4f6a7b8cf21165dce74be2149b28234f26
Zhenbo Li <litimetal@gmail.com>
2017-02-24 12:59:49 +0800

    First successful calc
    Signed-off-by: Zhenbo Li <litimetal@gmail.com>

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
 import pymontecarlo
 from deuces.deuces.card import Card
+from deuces.deuces.evaluator import Evaluator
+from deuces.deuces.lookup import LookupTable
+from itertools import combinations
 #from deuces import card
 
 def generate_set():
     suit = "shdc"
     rank = [str(i) for i in range(2,10)] + ['T', 'J', 'Q', 'K', 'A']
     l = (Card.new(b+a) for a in suit for b in rank)
-    return list(l)
+    return l
 
-Card.print_pretty_cards(generate_set()[0:10])
+deck = list(generate_set())
+hand = deck[5:7]
+deck = tuple(deck[:5] + deck[7:])
+#Card.print_pretty_cards(hand)
+#Card.print_pretty_cards(deck[:12])
+public_situations = tuple(combinations(deck, 5))
+evaluator = Evaluator()
+ret = dict()
+for i in range(1,10):
+    ret[i] = 0
+total_games = 0
+for i in public_situations:
+    board = list(i)
+    score = evaluator.evaluate(board, hand)
+    rank = evaluator.get_rank_class(score)
+    ret[rank] += 1
+    total_games += 1
+
+for k,v in ret.items():
+    pr = "{0}: {1} times, {2}%".format(evaluator.class_to_string(k), v, v/total_games*100.0)
+    print(pr)
